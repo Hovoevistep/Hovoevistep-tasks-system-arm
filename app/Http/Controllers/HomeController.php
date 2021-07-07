338500dd6f5d70c4b9eda3be6a0caba8c910148f
Hovoevistep <hovhannes.evistep@gmail.com>
2021-07-07 17:04:34 +0400

    commmit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TrelloCredential;
 use Illuminate\Http\Request;
+use Unirest;
 
 class HomeController extends Controller
 {
@@ -28,6 +29,21 @@ class HomeController extends Controller
 
     public function home(Request $request)
     {
+
+        // $_POST( 'https://api.trello.com/1/tokens/' . $token . '/webhooks/?key='. $key, {
+        //     callbackURL: 'http://127.0.0.1:8000/',
+        //     idModel: '60d97e978948481728008517',
+        //   });
+
+
+        $token = 'a71180e688e3be6b1ba8c7c7714b8f8baa9a55ba1e01baebc76c4251d1697d14';
+        $key = '765ea670a0fe9f0bb0fd7865732849bb';
+        $callbackURL = 'https://tasks-system-am.herokuapp.com/';
+        $idModel = '60d97e978948481728008517';
+        $responseWeb =  Unirest\Request::post('https://api.trello.com/1/tokens/' . $token . '/webhooks/?key=' . $key . '&idModel='. $idModel .'&callbackURL=' . $callbackURL);
+
+        dd($request->all());
+
         return view('auth.login');
     }
 

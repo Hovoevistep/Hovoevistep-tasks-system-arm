4fcb39bfa47f53419e64d88e8e1bbf44ffb7d461
Hovoevistep <hovhannes.evistep@gmail.com>
2021-07-13 12:12:23 +0400

    eli commit

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,19 @@ Route::middleware('auth')->group(function() {
 
 Route::post('/webhook', function (\Illuminate\Http\Request $request) {
     // csrf_token();
+
+
+
+    // $client= new GuzzleHttp\Client();
+    // $res=
+    // $client->request('POST','https://api.trello.com/1/webhooks/?key=765ea670a0fe9f0bb0fd7865732849bb&token=a71180e688e3be6b1ba8c7c7714b8f8baa9a55ba1e01baebc76c4251d1697d14&callbackURL=https://tasks-system-am.herokuapp.com/webhook&idModel=60d59b24d4b08e392f55ca73');
+    // dd($res->getBody());
+
+
+
+
+
+
     $response = Http::get('https://tasks-system-am.herokuapp.com/webhook');
     dump($response);
     $responsePost = Http::post('https://tasks-system-am.herokuapp.com/webhook');
